b691374c9cc297d3ff2695c729064d6a9694d468
alzy <gonzalezalfredo101@gmail.com>
2019-01-02 22:31:40 -0800

    Better handling of ENTER and ESC keys + added documentation to liveTextField.js

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # liveTextField.js
-Turns an element into a smart single line contenteditable field
+A simple function that converts an element into a single-line contenteditable field with specified event handlers.
+
+    <span id="text-block">Hello, World!</span>
+    
+    <script>
+    	let textBlock = document.querySelector('#text-block');
+    	liveTextField(
+    		textBlock,
+    		{
+    			'onfocus': () => {console.log('I recieved focus')},
+    			'oninput': () => {console.log('Someone is typing')},
+    			'onblur': () => {console.log('I lost focus')}
+    		}
+    	);
+    </script>
+
+Note that if the user presses the ESC key to leave the text field, the text will revert back to what it was before the user started editting.
--- liveTextField.js
+++ liveTextField.js
@@ -1,3 +1,9 @@
+/**
+ * Given an element and configuration object, convert the element into a
+ * content editable field and attach event handlers specified.
+ * @param  {domElement} element The dom element to convert
+ * @param  {JSON} config  An object containing event handlers (oninput, onfocus, onblur) 
+ */
 function liveTextField(element, config) {
 
 	this.init = function (element, config) {
@@ -12,19 +18,26 @@ function liveTextField(element, config) {
 		this.dom.onkeydown = (event) => {
 			// ENTER
 			if(event.which == 13){
+				this.dom.innerText = this.dom.innerText.trim().replace('\n', '')
 				this.config.oninput();
 				this.dom.blur();
 				return false;
 			}
 			// ESC
 			if(event.which == 27){
-				element.innerText = this.previousTextBuffer;
+				this.dom.innerText = this.previousTextBuffer;
 				this.dom.blur();
 				return false;
 			}
 		}
 	}
 
+	/**
+	 * Given a JSON config object, attach the event handlers to this object.
+	 * @param  {JSON} newConfig The object containing event handler definitions.
+	 *                          (oninput, onfocus, onblur). None are required. Will
+	 *                          set unspecified handlers as blank function by default.
+	 */
 	this.updateConfig = function (newConfig) {
 		newConfig = newConfig || {};
 
